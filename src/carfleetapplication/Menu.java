7a4393e7f0a9d77d244fd1275a0074313227dcc0
Damian Kalisz <kalidam>
2019-07-14 23:57:44 +0200

    refactioring, added test fo Car class

--- src/carfleetapplication/Menu.java
+++ src/carfleetapplication/Menu.java
@@ -11,7 +11,17 @@ public class Menu {
         System.out.println("    *********************************************");
         System.out.println("    *                   MENU                    *");
         System.out.println("    *********************************************");
-        showMenuOptions();
+        System.out.println("");
+        System.out.println("    1.Wczytaj wcześniej utworzone pliki.");
+        System.out.println("    2.Wyświetl listę pracowników.");
+        System.out.println("    3.Wyświetl listę samochodów.");
+        System.out.println("    4.Dodaj nowego pracownika.");
+        System.out.println("    5.Dodaj nowy pojazd.");
+        System.out.println("    6.Zmień kierowcę przypisanego do pojazdu.");
+        System.out.println("    7.Usuń pracownika z listy.");
+        System.out.println("    8.Usuń samochód z listy.");
+        System.out.println("");
+        System.out.println("    9.Zakończ program i zapisz wyniki pracy.");
         int selectedOption=ScannerUtility.getIntFromUser("Wybierz jedną z powyższych opcji poprzez podanie jej numeru.");
         return selectedOption;
     }
@@ -57,33 +67,6 @@ public class Menu {
         }
     }
     
-    public enum menuOptions {
-        LOAD_DATA("    1.Wczytaj wcześniej utworzone pliki."),
-        PRINT_EMPLOYEES("    2.Wyświetl listę pracowników."),
-        PRINT_CARS("    3.Wyświetl listę samochodów."),
-        ADD_EMPLOYEE("    4.Dodaj nowego pracownika."),
-        ADD_CAR("    5.Dodaj nowy pojazd."),
-        SAVE_DATA("    6.Zmień kierowcę przypisanego do pojazdu."),
-        REMOVE_EMPLOYEE("    7.Usuń pracownika z listy."),
-        REMOVE_CAR("    8.Usuń samochód z listy."),
-        EMPTY_LINE(""),
-        END_PROGRAM("    9.Zakończ program i zapisz wyniki pracy.");
-        
-        private String messageToShow;
-        menuOptions(String messageToshow) {
-        this.messageToShow=messageToshow;
-        }
-        
-        public String getMessageToShow() {
-            return this.messageToShow;
-        }
-    }
-    
-    public static void showMenuOptions() {
-        for(menuOptions message: menuOptions.values())
-            System.out.println(message.getMessageToShow());
-    }
-        
     private static void loadData(){
         LoadFileEmployee loadFileEmployee=new LoadFileEmployee();
         loadFileEmployee.getFileLocationFromUserAndMakeIOOperaation();
---
+++ test/carfleetapplication/CarTest.java
@@ -0,0 +1,195 @@
+package carfleetapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class CarTest {
+    
+    public CarTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of setListOfEmploies method, of class Car.
+     */
+    @Test
+    public void testSetListOfEmploies() {
+        System.out.println("setListOfEmploies");
+        Car instance = null;
+        instance.setListOfEmploies();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDriverFromListOfEmployees method, of class Car.
+     */
+    @Test
+    public void testSetDriverFromListOfEmployees() {
+        System.out.println("setDriverFromListOfEmployees");
+        int employeeNumber = 0;
+        Car instance = null;
+        instance.setDriverFromListOfEmployees(employeeNumber);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setBrand method, of class Car.
+     */
+    @Test
+    public void testSetBrand() {
+        System.out.println("setBrand");
+        String brand = "";
+        Car instance = null;
+        instance.setBrand(brand);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setModel method, of class Car.
+     */
+    @Test
+    public void testSetModel() {
+        System.out.println("setModel");
+        String model = "";
+        Car instance = null;
+        instance.setModel(model);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRegistrationNumber method, of class Car.
+     */
+    @Test
+    public void testSetRegistrationNumber() {
+        System.out.println("setRegistrationNumber");
+        String registrationNumber = "";
+        Car instance = null;
+        instance.setRegistrationNumber(registrationNumber);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBrand method, of class Car.
+     */
+    @Test
+    public void testGetBrand() {
+        System.out.println("getBrand");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.getBrand();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getModel method, of class Car.
+     */
+    @Test
+    public void testGetModel() {
+        System.out.println("getModel");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.getModel();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRegistrationNumber method, of class Car.
+     */
+    @Test
+    public void testGetRegistrationNumber() {
+        System.out.println("getRegistrationNumber");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.getRegistrationNumber();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDriver method, of class Car.
+     */
+    @Test
+    public void testGetDriver() {
+        System.out.println("getDriver");
+        Car instance = null;
+        Employee expResult = null;
+        Employee result = instance.getDriver();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class Car.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSurname method, of class Car.
+     */
+    @Test
+    public void testGetSurname() {
+        System.out.println("getSurname");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.getSurname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of changeDriver method, of class Car.
+     */
+    @Test
+    public void testChangeDriver() {
+        System.out.println("changeDriver");
+        int changedEmloyeeNumber = 0;
+        Car instance = null;
+        instance.changeDriver(changedEmloyeeNumber);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Car.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Car instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
