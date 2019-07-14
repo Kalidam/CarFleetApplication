2543a8176db8694a72900b7d05092808959e6380
Damian Kalisz <kalidam>
2019-07-14 19:46:46 +0200

    added enum method to show menu options

--- src/carfleetapplication/Menu.java
+++ src/carfleetapplication/Menu.java
@@ -11,15 +11,7 @@ public class Menu {
         System.out.println("    *********************************************");
         System.out.println("    *                   MENU                    *");
         System.out.println("    *********************************************");
-        System.out.println("    1.Wczytaj wcześniej utworzone pliki.");
-        System.out.println("    2.Wyświetl listę pracowników.");
-        System.out.println("    3.Wyświetl listę samochodów.");
-        System.out.println("    4.Dodaj nowego pracownika.");
-        System.out.println("    5.Dodaj nowy pojazd.");
-        System.out.println("    6.Zmień kierowcę przypisanego do pojazdu.");
-        System.out.println("");
-        System.out.println("    7. Zakończ program i zapisz wyniki pracy.");
-        System.out.println("");
+        showMenuOptions();
         int selectedOption=ScannerUtility.getIntFromUser("Wybierz jedną z powyższych opcji poprzez podanie jej numeru.");
         return selectedOption;
     }
@@ -57,6 +49,31 @@ public class Menu {
         }
     }
     
+    public enum menuOptions {
+        LOAD_DATA("    1.Wczytaj wcześniej utworzone pliki."),
+        PRINT_EMPLOYEES("    2.Wyświetl listę pracowników."),
+        PRINT_CARS("    3.Wyświetl listę samochodów."),
+        ADD_EMPLOYEE("    4.Dodaj nowego pracownika."),
+        ADD_CAR("    5.Dodaj nowy pojazd."),
+        SAVE_DATA("    6.Zmień kierowcę przypisanego do pojazdu."),
+        EMPTY_LINE(""),
+        END_PROGRAM("    7.Zakończ program i zapisz wyniki pracy.");
+        
+        private String messageToShow;
+        menuOptions(String messageToshow) {
+        this.messageToShow=messageToshow;
+        }
+        
+        public String getMessageToShow() {
+            return this.messageToShow;
+        }
+    }
+    
+    public static void showMenuOptions() {
+        for(menuOptions message: menuOptions.values())
+            System.out.println(message.getMessageToShow());
+    }
+        
     private static void loadData(){
         LoadFileEmployee loadFileEmployee=new LoadFileEmployee();
         loadFileEmployee.getFileLocationFromUserAndMakeIOOperaation();
